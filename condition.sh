4a7c72aa6c1bd6fa9efcfb7a5f7e8ebcb4237c8b
vyshnavi <vyshnavichennagari2@gmail.com>
2026-07-17 13:53:15 +0530

    hello world Vyshu

--- condition.sh
+++ condition.sh
@@ -2,14 +2,14 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ] then
+if [ $USERID -ne 0 ]; then
  echo " you are running with root access "
- 
+
 fi
 
 dnf install mysql
 
-if [ $? -eq 0 ] then
+if [ $? -eq 0 ]; then
  echo "Myqsl is getting installed"
 
 fi
