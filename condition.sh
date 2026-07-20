14d892fb74989a2f923c84ea4536f020286beb1a
vyshnavi <vyshnavichennagari2@gmail.com>
2026-07-20 16:48:29 +0530

    hello world Vyshu

--- condition.sh
+++ condition.sh
@@ -2,25 +2,25 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ] then
+if [ $USERID -ne 0 ] ; then
  echo "Print you are not root user"
  exit 12
- else 
+else 
  echo "You are running as root user"
 fi
 
 dnf install python3
 
-if [ $? -eq 0 ] then
-echo " Installing Python 3"
+if [ $? -eq 0 ] ; then
+ echo " Installing Python 3"
 else
  echo " Not installed Python 3"
  exit 12
 fi
 
 dnf list installed python3 nginx 
- if [ $? -eq 0 ] then
- echo " installing python3...."
+ if [ $? -eq 0 ] ; then
+  echo " installing python3...."
  else 
  echo " python3 is not installled.."
  exit 13
