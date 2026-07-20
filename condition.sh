333cf0258f9b1ca11dad650877050de388c948c3
vyshnavi <vyshnavichennagari2@gmail.com>
2026-07-20 17:41:23 +0530

    hello world Vyshu

--- condition.sh
+++ condition.sh
@@ -22,37 +22,52 @@ VALIDATE() {
 
 }
 
-dnf list  installed python
+# dnf list  installed python
 
-if [ $? -eq 0 ] ; then
- echo " Installing Python "
- dnf  install  python 
- VALIDATE $? "python"
-else
- echo " Not installed Python "
- exit 12
-fi
-
-# dnf list installed python3 
-#  if [ $? -eq 0 ] ; then
-#   echo " installing python3...."
-#  else 
-#  echo " python3 is not installled.."
-#  exit 13
+# if [ $? -eq 0 ] ; then
+#  echo " Installing Python "
+#  dnf  install  python 
+#  VALIDATE $? "python"
+# else
+#  echo " Not installed Python "
+#  exit 12
 # fi
 
-dnf list installed nginx
- if [ $? -eq 0 ] ; then 
- echo " nginx going to install  tooo."
-dnf install nginx
-VALIDATE $? "nginx"
+# # dnf list installed python3 
+# #  if [ $? -eq 0 ] ; then
+# #   echo " installing python3...."
+# #  else 
+# #  echo " python3 is not installled.."
+# #  exit 13
+# # fi
 
- else
-  echo " not installed ."
-  exit 12
+# dnf list installed nginx
+#  if [ $? -eq 0 ] ; then 
+#  echo " nginx going to install  tooo."
+# dnf install nginx
+# VALIDATE $? "nginx"
+
+#  else
+#   echo " not installed ."
+#   exit 12
+# fi
+
+dnf list installed python3
+
+if [ $? -ne 0 ]; then
+    echo "Installing Python3..."
+    dnf install -y python3
+    VALIDATE $? "python3"
+else
+    echo "Python3 is already installed."
 fi
 
+dnf list installed nginx
 
-
-
-   
+if [ $? -ne 0 ]; then
+    echo "Installing nginx..."
+    dnf install -y nginx
+    VALIDATE $? "nginx"
+else
+    echo "Nginx is already installed."
+fi
