d3cebf9b647baa11c71bb3f24a923ea29b838ea9
vyshnavi <vyshnavichennagari2@gmail.com>
2026-07-20 17:17:06 +0530

    hello world Vyshu

--- condition.sh
+++ condition.sh
@@ -1,31 +1,56 @@
 #!/bin/bash
 
 USERID=$(id -u)
+R=\e[31m'
+Y=\3[33m'
+N=\e[0m'
 
 if [ $USERID -ne 0 ] ; then
- echo "Print you are not root user"
+ echo  -e " $R Print you are not root user $N"
  exit 12
 else 
- echo "You are running as root user"
+ echo "$Y You are running as root user $N"
 fi
 
-dnf install python3
+VALIDATE() {
+     if [ $? -eq 0 ] ; then
+       echo " installing python3...."
+     else 
+       echo " python3 is not installled.."
+     exit 13
+fi
+
+}
+
+dnf list  installed python3X
 
 if [ $? -eq 0 ] ; then
  echo " Installing Python 3"
+ dnf  install  python3 
+ VALIDATE $? $1
 else
  echo " Not installed Python 3"
  exit 12
 fi
 
-dnf list installed python3 nginx 
- if [ $? -eq 0 ] ; then
-  echo " installing python3...."
- else 
- echo " python3 is not installled.."
- exit 13
-fi
+# dnf list installed python3 
+#  if [ $? -eq 0 ] ; then
+#   echo " installing python3...."
+#  else 
+#  echo " python3 is not installled.."
+#  exit 13
+# fi
 
+dnf list installed nginx
+ if [ $? -eq 0 ] ; then 
+ echo " nginx going to install  tooo."
+dnf install nginx
+VALIDATE $? $2
+
+ else
+  echo " not installed ."
+  exit 12
+fi
 
 
 
