f8465967c9810fb30436f817169144658f044d42
vyshnavi <vyshnavichennagari2@gmail.com>
2026-07-20 16:42:31 +0530

    hello world Vyshu

--- condition.sh
+++ condition.sh
@@ -2,17 +2,32 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]; then
- echo " you are running with root access "
-
+if [ $USERID -ne 0 ] then
+ echo "Print you are not root user"
+ exit 12
+else 
+ echo "You are running as root user"
 fi
 
-dnf install mysql
+dnf install python3
 
-if [ $? -eq 0 ]; then
- echo "Myqsl is getting installed"
-
+if [ $? -eq 0 ] then
+echo " Installing Python 3"
+else
+ echo " Not installed Python 3"
+ exit 12
 fi
+
+dnf list installed python3 nginx 
+ if [ $? -eq 0 ] then
+ echo " installing python3...."
+ else 
+ echo " python3 is not installled.."
+ exit 13
+fi
+
+
+
 
 
    
