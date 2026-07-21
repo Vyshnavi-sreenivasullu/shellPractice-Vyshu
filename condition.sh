cbf7e8a5a7a13394f0e611ff3f41ab220d2863f9
vyshnavi <vyshnavichennagari2@gmail.com>
2026-07-21 15:59:43 +0530

    hello world Vyshu

--- condition.sh
+++ condition.sh
@@ -4,12 +4,12 @@ USERID=$(id -u)
 R="\e[31m"
 Y="\e[33m"
 N="\e[0m"
-LOGS_FOLDER="/var/log/shellscript-logs"
-SCRIPT_NAME=$(echo $0 | cut  -d "." - f1)
-LOG_FILE="$LOG_FILE/$SCRIPT_NAME. log"
 # LOGS_FOLDER="/var/log/shellscript-logs"
-#  SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-# LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+# SCRIPT_NAME=$(echo $0 | cut  -d "." - f1)
+# LOG_FILE="$LOG_FILE/$SCRIPT_NAME. log"
+ LOGS_FOLDER="/var/log/shellscript-logs"
+ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 
