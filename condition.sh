bedbad607612685693ced9c20a92ec70779a5021
vyshnavi <vyshnavichennagari2@gmail.com>
2026-07-21 12:50:45 +0530

    hello world Vyshu

--- condition.sh
+++ condition.sh
@@ -5,10 +5,10 @@ R="\e[31m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
-SCRIPT_NAME=$(basename "$0" .sh)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+# SCRIPT_NAME=$(basename "$0" .sh)
 # LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 
@@ -48,7 +48,6 @@ if [ $? -ne 0 ]; then
  VALIDATE $? "nginx"
 else
   echo -e "${R} already installed ${N} ." &>>$LOG_FILE
-  exit 12
 fi
 
 
